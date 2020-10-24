7e3c8fb1bd006d07b5738ea74cddacea83dd2cdc
Yael Dauber <yaeldau@gmail.com>
2020-10-24 10:22:56 +0300

    lab 16- ex 3

--- react-16/src/main.js
+++ react-16/src/main.js
@@ -4,13 +4,15 @@ import ReactDOM from 'react-dom';
 import '../css/main.css';
 import CatchTheRedGame from "./CatchTheRedGame";
 import FilterList from "./FilterList";
+import Form from "./Form";
 
 const App = () => {
 
   return (
       <div>
         {/*<CatchTheRedGame/>*/}
-        <FilterList/>
+        {/*<FilterList/>*/}
+        <Form />
       </div>
   )
 };
